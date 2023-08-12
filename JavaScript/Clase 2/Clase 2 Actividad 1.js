92f26cbf3f074c45baf8c3198667dc905d06be08
TiagoRoccatagliata <tiagoroccatagliata@gmail.com>
2023-08-11 23:31:11 -0300

    a

---
+++ JavaScript/Clase 2/Clase 2 Actividad 1.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Introduce tu nombre");
+var edad = prompt("Introduce tu edad");
+
+
+
+
+console.log("Tu nombre es: " + nombre + "y tu edad es: " + edad);
---
+++ JavaScript/Clase 2/Clase 2 Actividad 2.js
@@ -0,0 +1,12 @@
+
+let nombreIngresado = prompt("Por favor, ingresa tu nombre:");
+
+
+let miNombre = "AI";
+
+
+if (nombreIngresado === miNombre) {
+  console.log("¡Éxito! Has ingresado mi nombre.");
+} else {
+  console.log("Error: El nombre ingresado no coincide con el mío.");
+}
